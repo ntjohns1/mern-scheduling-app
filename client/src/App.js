d6f3f14c198964dd3155ce26588db017b57a9955
ntjohns1 <nelsontjohns@gmail.com>
2021-08-20 03:37:15 -0400

    set up client side file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {
 	ApolloClient,
 	InMemoryCache,
@@ -8,16 +8,6 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from './components/Navbar';
-import Product from './pages/store';
-import Shopkeeper from './components/Shopkeeper';
-import AddItem from './pages/addItem';
-import Contact from './components/Contact';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Profile from './pages/profile';
-import Footer from './components/Footer';
-import SingleProduct from './pages/SingleProduct';
-import { keepTheme } from './components/themes';
 
 const httpLink = createHttpLink({
 	uri: '/graphql',
@@ -42,38 +32,17 @@ const client = new ApolloClient({
 });
 
 export default function App() {
-	useEffect(() => {
-		keepTheme();
-	})
 	return (
 		<ApolloProvider client={client}>
 			<Router>
 				<div className='impDiv'>
 					{/* <Header /> */}
 					<Navigation />
-					<Route exact path='/profile'>
-						<Profile />
-					</Route>
 					<Route exact path='/'>
-						<Product />
-					</Route>
-					<Route exact path='/addItem'>
-						<AddItem />
-					</Route>
-					<Route exact path='/shopkeeper'>
-						<Shopkeeper />
-					</Route>
-					<Route exact path='/contact'>
-						<Contact />
+						<Homepage />
 					</Route>
 					<Route exact path='/login'>
 						<Login />
-					</Route>
-					<Route exact path='/signup'>
-						<Signup />
-					</Route>
-					<Route exact path='/:productId'>
-						<SingleProduct />
 					</Route>
 					<Footer />
 				</div>
